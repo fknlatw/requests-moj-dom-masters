86ecc511559ae7d376ef2a1f2b0454fcffaba3d4
fknlatw <argat2480@gmail.com>
2026-07-16 18:35:56 +0200

    -add/added header scroll listener

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
 import s from "./header.module.css";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../context/AuthContext.jsx";
 import { FiLogIn, FiLogOut  } from "react-icons/fi";
 import { Link } from "react-router-dom";
@@ -7,9 +7,24 @@ import { Button } from "../UI/Button/Button.jsx";
 
 
 export const Header = () => {
+  const [scroll, setScroll] = useState(false);  
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScroll(window.scrollY > 0);
+    }
+
+    window.addEventListener("scroll", handleScroll);
+
+    handleScroll();
+
+    return () => {
+      window.removeEventListener("scroll",handleScroll);
+    }
+  }, []);
   const {logout, user} = useContext(AuthContext);
   return (
-    <header className={`${s.header}`}>
+    <header className={`${scroll && "glass"} ${s.header}`}>
       <div className={s.headerContainer}>
         {user
         ? <Button onClick={logout}><FiLogOut/></Button> :<Link to="/login" ><Button><FiLogIn/></Button></Link>}
