d9538cdff6c16b547835ce33509b246061234a2b
fknlatw <argat2480@gmail.com>
2026-07-17 23:42:27 +0200

    -add/header buttons

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,23 @@ import { MainPage } from "./pages/MainPage.jsx";
 import { Routes, Route } from "react-router-dom";
 import { Header } from "./components/Header/Header.jsx";
 import { LoginForm } from "./components/LoginForm/LoginForm.jsx";
-import {RequestPage} from "./pages/RequestPage.jsx";
+import { RequestPage } from "./pages/RequestPage.jsx";
+import { useState } from "react";
 
 
 export const App = () => {
-  
-
+  const [isOpen, setIsOpen] = useState({calculator: false, filters: false, addRequest: false});
   return (
-    <main className="mainContainer">
-      <Header />
-
-      <Routes>
-        <Route path="/" element={<MainPage/>}/>
-        <Route path="/login" element={<LoginForm/>}/>
-        <Route path="/request/:id" element={<RequestPage/>}/>
-      </Routes>
-    </main>
+    <>
+      <Header setIsOpen={setIsOpen} isOpen={isOpen} />
+      <main className="mainContainer">
+        
+        <Routes>
+          <Route path="/" element={<MainPage isOpen={isOpen} />} />
+          <Route path="/login" element={<LoginForm />} />
+          <Route path="/request/:id" element={<RequestPage />} />
+        </Routes>
+      </main>
+    </>
   );
 };
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,36 +1,67 @@
 import s from "./header.module.css";
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../context/AuthContext.jsx";
-import { FiLogIn, FiLogOut  } from "react-icons/fi";
+import { FiLogIn, FiLogOut } from "react-icons/fi";
 import { Link } from "react-router-dom";
 import { Button } from "../UI/Button/Button.jsx";
+import { FaCalculator, FaFilter, FaPlusCircle } from "react-icons/fa";
 
-
-export const Header = () => {
-  const [scroll, setScroll] = useState(false);  
+export const Header = ({ setIsOpen, isOpen }) => {
+  const [scroll, setScroll] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
       setScroll(window.scrollY > 0);
-    }
+    };
 
     window.addEventListener("scroll", handleScroll);
 
     handleScroll();
 
     return () => {
-      window.removeEventListener("scroll",handleScroll);
-    }
+      window.removeEventListener("scroll", handleScroll);
+    };
   }, []);
-  const {logout, user} = useContext(AuthContext);
+  const { logout, user } = useContext(AuthContext);
   return (
     <header className={`${scroll && "glass"} ${s.header}`}>
       <div className={s.headerContainer}>
-        {user
-        ? <Button onClick={logout}><FiLogOut/></Button> :<Link to="/login" ><Button><FiLogIn/></Button></Link>}
-      
+        {user ? (
+          <Button onClick={logout}>
+            <FiLogOut />
+          </Button>
+        ) : (
+          <Link to="/login">
+            <Button>
+              <FiLogIn />
+            </Button>
+          </Link>
+        )}
+        <Button
+          onClick={() => setIsOpen({ ...isOpen, filters: !isOpen.filters })}
+        >
+          <FaFilter />
+        </Button>
 
+        {user && (
+          <>
+            <Button
+              onClick={() =>
+                setIsOpen({ ...isOpen, calculator: !isOpen.calculator })
+              }
+            >
+              <FaCalculator />
+            </Button>
+            <Button
+              onClick={() =>
+                setIsOpen({ ...isOpen, addRequest: !isOpen.addRequest })
+              }
+            >
+              <FaPlusCircle />
+            </Button>
+          </>
+        )}
       </div>
     </header>
-  )
-}
+  );
+};
